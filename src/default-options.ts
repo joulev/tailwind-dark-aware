9b22c1a99ac4af05d83b67cd0daa2de38be2089e
Vu Van Dung <me@joulev.dev>
2022-10-15 17:46:20 +0800

    rename default suffix from wd to daw
    someone helps me naming these
    
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/default-options.ts
+++ src/default-options.ts
@@ -1,7 +1,7 @@
 import type { Options } from "$types";
 
 const options: Options = {
-  suffix: "wd",
+  suffix: "daw",
   ignoredKeys: ["50"],
   invertCustomColours: true,
   invertDefaultColours: false,
